ef45e24afa90f09d50608b4ff2a07488f2b5bbec
gladys porfirio <gladysporfirio@gladyss-MacBook-Air.local>
2024-01-08 14:30:49 -0500

    first commit

---
+++ my_code.py
@@ -0,0 +1,11 @@
+def main() :
+    name = "Gladys Porfirio"
+    print("We want to know if you like prograamming! ")
+    print()
+    print("Do you like programming "+ name + "?")
+    answer = input()
+    print("Great! You said " + answer + "?")
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__" :
+    main()
---
+++ my_code_2.py
@@ -0,0 +1,9 @@
+def main() :
+    name = input("What is your name ? ")
+    print("We want to know if you like programming? ")
+    answer = input("Do you like programming "+ name + "? ")
+    print('Great! You said "' + answer+ '" ?')
+    print("Let's start learning some Python today !")
+
+if __name__ == "__main__" :
+    main()
