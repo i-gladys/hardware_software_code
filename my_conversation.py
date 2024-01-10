5c0564cd5fbcb7a99f7142b7d6234ab9f1d1a009
gladys porfirio <gladysporfirio@gladyss-MacBook-Air.local>
2024-01-10 13:24:44 -0500

    20240110 H+S Class

---
+++ my_conversation.py
@@ -0,0 +1,18 @@
+def conversation():
+    print("Do you like coding in python? Answer yes or no !")
+    answer = input()
+    if (answer == "yes" or answer == "Yes"):
+        print("That's good - the United States needs more coders !!")
+    elif (answer == "no" or answer == "No"):
+        print("Perhaps you will change your mind .")
+    else:
+        print("I do not understand.")
+    print("Goodbye !")
+
+def main():
+    print("Welcome to my conversation program .")
+    conversation()
+    print("Thanks for chatting with me !")
+
+if __name__ == "__main__":
+    main()
